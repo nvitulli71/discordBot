7b96517e3e0944970d78873198a30ac5ce89a9db
Nick <nick@nickvitulli.dev>
2024-05-13 22:59:52 -0400

    created serivce to handle data manipulation and file writing

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const { Client, Events, GatewayIntentBits } = require("discord.js");
 const { token } = require("./config.json");
+const { setChannel, addContent } = require("./services/general.service");
 
-const COMMAND_PREFIX = "!content_in";
+const COMMAND_PREFIX = "!";
 
 // Create a new client instance
 const client = new Client({
@@ -19,10 +20,47 @@ client.once(Events.ClientReady, (readyClient) => {
 
 client.login(token);
 
-client.on("messageCreate", (message) => {
+client.on("messageCreate", async (message) => {
+  const serverId = message.guild.id;
+  const channelId = message.channel.id;
+  const messageId = message.id;
+
   if (message.author.bot) return;
   if (!message.content || !message.content.startsWith(COMMAND_PREFIX)) return;
   if (message.content.startsWith(COMMAND_PREFIX)) {
+    switch (true) {
+      case message.content.startsWith(`${COMMAND_PREFIX}set_channel`):
+        let channelId = message.content.replace(/\D/g, "");
+        await setChannel(channelId)
+          .catch(() => {
+            message.react("❌");
+            message.reply(
+              "There was an issue setting the channel to display content..."
+            );
+          })
+          .finally(() => {
+            message.react("✅");
+            message.reply("Channel was set...");
+          });
+      case message.content.startsWith(`${COMMAND_PREFIX}content_in`):
+        let content = message.content;
+        await addContent(content)
+          .catch(() => {
+            message.react("❌");
+            message.reply("There was an issue adding in content...");
+          })
+          .finally(message.react("✅"));
+      case message.content.startsWith(`${COMMAND_PREFIX}all_content`):
+      // Something here
+      default:
+        return;
+    }
+
     //Do logic here to store the string value
+
+    // Need a command that will hold the channel we need to be watching
+    // Need a service here that we will pass the serverId and message into
+    // Will need to parse the message for time and type of content
+    // Will need to keep a timer somewhere to check every so often (maybe at the top of each hour and a command to show content comming up)
   }
 });
---
+++ services/general.service.js
@@ -0,0 +1,44 @@
+const fs = require("fs");
+const path = require("path");
+
+async function writeFile(fileName, content) {
+  var jsonPath = path.join(__dirname, "..", "data", `${fileName}`);
+
+  await fs.writeFile(jsonPath, JSON.stringify(content), (error, data) => {
+    if (error) {
+      throw new Error("File write Error");
+    }
+  });
+}
+
+async function setChannel(channelId) {
+  const data = {};
+  data.channelId = channelId;
+  await writeFile("channel.json", data);
+}
+
+function getChannel() {}
+
+function parseContent(contentString) {
+  const parseContent = contentString.split(" ");
+
+  return {
+    time: parseContent[1],
+    content: parseContent.slice(2).join(" "),
+  };
+}
+
+async function addContent(contentString) {
+  console.log(contentString);
+
+  const { time, content } = parseContent(contentString);
+  console.log(time, content);
+}
+
+function deleteContent(contentId) {}
+
+function displayContent() {}
+
+function checkForContent() {}
+
+module.exports = { setChannel, addContent };
